9ad86549071bd878bfd8c56ec50b6ee3c15a24bd
Caleb James DeLisle <cjd@cjdns.fr>
2017-06-23 18:52:22 +0200

    Use blob type in sql to allow emoji in usernames

--- initdb.sql
+++ initdb.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE hubot;
-CREATE USER 'hubot'@'localhost' IDENTIFIED BY 'b957f529861ffbe48c7b34bce651faa8';
+CREATE DATABASE IF NOT EXISTS hubot;
+CREATE USER IF NOT EXISTS 'hubot'@'localhost' IDENTIFIED BY 'b957f529861ffbe48c7b34bce651faa8';
 GRANT ALL PRIVILEGES ON hubot.* TO 'hubot'@'localhost';
 USE hubot;
-CREATE TABLE `brain` (
+CREATE TABLE IF NOT EXISTS `brain` (
   `id` INT,
-  `data` TEXT,
+  `data` BLOB,
   PRIMARY KEY (`id`)
 );
